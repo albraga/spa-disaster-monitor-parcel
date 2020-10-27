10fa9a1050d885ea229603e4aba306f6f68b4d85
Alexandre Braga <albraga@email.com>
2020-10-26 22:33:57 -0400

    .

--- src/js/api.js
+++ src/js/api.js
@@ -1,13 +1,13 @@
 import countries from './countries'
-import axios from 'axios' 
+import superagent from 'superagent'
 
 const URL = 'https://api.reliefweb.int/v1/disasters?appname=omundoagora&profile=full&offset=0&limit=10&preset=latest'
 
 function getAffectedCountries() {
   let affectedCountries = []
-  return axios.get(URL).then(result => {
-    result.data.data.forEach(disaster => {
-      affectedCountries.push(countries.find( country => country.iso3 == disaster.fields.country[0].iso3))
+  return superagent.get(URL).then(result => {
+    result.body.data.forEach(disaster => {
+      affectedCountries.push(countries.find(country => country.iso3 == disaster.fields.country[0].iso3))
     })
     return affectedCountries
   })
